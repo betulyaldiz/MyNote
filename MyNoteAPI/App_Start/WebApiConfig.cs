8e6d24c6846dd41d9c5c76eba1c40bbd05edad2a
Betül Yaldız <betulbulut94@gmail.com>
2020-06-05 15:46:29 +0300

    Angular

--- MyNoteAPI/App_Start/WebApiConfig.cs
+++ MyNoteAPI/App_Start/WebApiConfig.cs
@@ -12,13 +12,21 @@ namespace MyNoteAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            //https://stackoverflow.com/questions/7397207/json-net-error-self-referencing-loop-detected-for-type
+
+            // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling
+                = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes();            
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
